624fe8347ee1b5e8a22aa78e7c027dfe66e1693f
Damien Duhamel <d.damien@live.fr>
2022-06-12 13:44:39 +0200

    Add scan

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
+
+function log(message) {
+  const result = document.querySelector('#result');
+
+  result.innerHTML += `
+    > ${message}
+  `;
+}
+
 function onClick() {
-  const result = document.querySelector("#result");
-  result.innerHTML = 'NDEFReader' in window ? 'enabled' : 'disabled';
+  try {
+    const ndef = new NDEFReader();
+
+    await ndef.scan();
+
+    log('Scan started');
+
+    ndef.addEventListener('reading', ({ message, serialNumber }) => {
+      log(`Serial Number: ${serialNumber}`);
+      log(`Records: (${message.records.length})`);
+      for (const record of message.records) {
+        log(`Record type: ${record.recordType}`);
+        log(`MIME type: ${record.mediaType}`);
+        log(`Record id: ${record.id}`);
+      }
+    });
+  } catch (error) {
+    log('Argh! ' + error);
+  }
 }
 
 function main() {
-  const button = document.querySelector("#button");
+  const button = document.querySelector('#button');
   button.addEventListener('click', onClick);
 }
 
